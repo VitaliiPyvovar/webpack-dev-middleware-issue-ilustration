d42134f0eee63f09f40c229023ba24807ab48c1f
Vitalii Pyvovar <vitalii@openvpn.net>
2021-03-09 17:43:31 +0200

    Update webpack-dev-middleware to 4.1.0

--- server/middlewares/addDevMiddlewares.js
+++ server/middlewares/addDevMiddlewares.js
@@ -7,19 +7,14 @@ const entryPoint = require('./entryPoint');
 module.exports = function addDevMiddlewares(app, webpackConfig) {
     const compiler = webpack(webpackConfig);
     const middleware = webpackDevMiddleware(compiler, {
-        // todo remove all options after webpack-dev-middleware version update
-        publicPath: '/static/',
         stats: 'errors-only',
-        noInfo: true,
-        silent: true,
     });
 
     app.use(middleware);
     app.use(webpackHotMiddleware(compiler));
 
-    // todo use middleware.context.outputFileSystem after webpack-dev-middleware version update
-    // const fs = middleware.context.outputFileSystem;
-    const fs = middleware.fileSystem;
+    const fs = middleware.context.outputFileSystem;
+    // const fs = middleware.fileSystem;
 
     app.get('*', (req, res) => {
         const filePath = entryPoint(req, compiler.outputPath);
